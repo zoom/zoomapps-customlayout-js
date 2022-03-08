6081b77c422564d7296fd8986340fa56ff1a2bd4
Max Mansfield <max.mansfield@zoom.us>
2022-03-08 14:19:00 -0700

    added drawImage func

--- public/js/index.js
+++ public/js/index.js
@@ -4,16 +4,44 @@ async function configure() {
     return zoomSdk.config({
         size: { width: 480, height: 360 },
         capabilities: [
-            /* Add Capabilities Here */
-            'shareApp',
+            'getRunningContext',
+            'runRenderingContext',
+            'closeRenderingContext',
+            'drawParticipant',
+            'clearParticipant',
+            'drawImage',
+            'clearImage',
+            'onMyMediaChange',
         ],
+    });
+}
+
+async function drawImage(src, x, y, zIndex) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = async () => {
+            const drawCtx = await zoomSdk.drawImage({
+                imageData: img,
+                x,
+                y,
+                zIndex,
+            });
+
+            resolve(drawCtx);
+        };
+
+        img.onerror = (e) => reject(e);
     });
 }
 
 (async () => {
     try {
         const configResponse = await configure();
-        console.debug('Zoom JS SDK Configuration', configResponse);
+        console.debug('configuration', configResponse);
+
+        const ctx = await drawImage('/img/zoom.png');
+        console.debug('Image Drawn', JSON.stringify(ctx));
     } catch (e) {
         console.error(e);
     }
