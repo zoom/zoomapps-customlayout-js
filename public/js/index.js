169b77cb3da652be657c13daf404c73d51e0a977
Max Mansfield <max.mansfield@zoom.us>
2022-03-08 19:26:44 -0700

    added logic to render participants

--- public/js/index.js
+++ public/js/index.js
@@ -5,6 +5,7 @@ async function configure() {
         size: { width: 480, height: 360 },
         capabilities: [
             'getRunningContext',
+            'getUserContext',
             'runRenderingContext',
             'closeRenderingContext',
             'drawParticipant',
@@ -12,26 +13,72 @@ async function configure() {
             'drawImage',
             'clearImage',
             'onMyMediaChange',
+            'onMeeting',
         ],
     });
 }
 
-async function drawImage(src, x, y, zIndex) {
+async function startRender(viewMode) {
+    // immersive mode can only be used in-meeting
+    const appCtx = await zoomSdk.getRunningContext();
+    if (appCtx !== 'inMeeting') return Promise.resolve(false);
+
+    return zoomSdk.runRenderingContext({ view: viewMode });
+}
+
+async function stopRender() {
+    return zoomSdk.closeRenderingContext();
+}
+
+/*async function drawStaticImage(src, x = "0px", y = "0px", zIndex = 1) {
     return new Promise((resolve, reject) => {
         const img = new Image();
-        img.src = src;
-        img.onload = async () => {
-            const drawCtx = await zoomSdk.drawImage({
-                imageData: img,
+
+        img.onload = function () {
+            const r = zoomSdk.drawImage({
+                imageData: this,
                 x,
                 y,
-                zIndex,
+                zIndex
             });
-
-            resolve(drawCtx);
+            resolve(r);
         };
 
         img.onerror = (e) => reject(e);
+        img.src = src;
+    });
+}*/
+
+async function drawParticipant(
+    participantId,
+    x = '0px',
+    y = '0px',
+    zIndex = 1,
+    width = '100%',
+    height = '100%'
+) {
+    return zoomSdk.drawParticipant({
+        participantId,
+        x,
+        y,
+        width,
+        height,
+        zIndex,
+    });
+}
+
+async function addEventListeners() {
+    await zoomSdk.addEventListener('onMeeting', async (eventInfo) => {
+        switch (eventInfo.action) {
+            case 'started':
+                await startRender('immersive');
+                break;
+            case 'ended':
+                await stopRender();
+                break;
+            default:
+                break;
+        }
     });
 }
 
@@ -40,9 +87,24 @@ async function drawImage(src, x, y, zIndex) {
         const configResponse = await configure();
         console.debug('configuration', configResponse);
 
-        const ctx = await drawImage('/img/zoom.png');
-        console.debug('Image Drawn', JSON.stringify(ctx));
+        await startRender('immersive');
+
+        // immersive mode can only be started by the host
+        const user = await zoomSdk.getUserContext();
+
+        if (user.role !== 'host') return false; // send app invitation here
+
+        await addEventListeners();
+
+        await drawParticipant(
+            user.participantId,
+            null,
+            '-100vh',
+            null,
+            '50%',
+            '50%'
+        );
     } catch (e) {
-        console.error(e);
+        console.error('Immersive mode failure', e);
     }
 })();
